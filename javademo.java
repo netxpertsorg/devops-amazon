c43edd574ac68e685f9fee32b75c8ea8b5dfa23a
kamal <kamal@gmail.com>
2023-09-04 15:59:00 +0000

    Javademo.java Modified

--- javademo.java
+++ javademo.java
@@ -2,3 +2,5 @@ System.out.println("Hello World!");
 System.out.println("I am learning Java.");
 System.out.println("It is awesome!");
 System.out.println(This sentence will produce an error);
+String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+System.out.println("The length of the txt string is: " + txt.length());
